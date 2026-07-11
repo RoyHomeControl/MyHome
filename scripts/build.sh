c9e4c3036d0ddd3249339f5ffa589c790d2e0a18
천성원 <chun_sungwon@naver.com>
2026-07-11 17:32:32 +0900

    빌드 결과 경로 수정, 마운트 경로 수정

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 cd /workspace
+echo "=== WorkSpace ==="
+pwd
+ls -al
 flutter pub get
 flutter build apk --release
+echo "=== Build Output ==="
+ls -al build/app/outputs/flutter-apk
+echo "=== Output Before ==="
+ls -al /output
 cp build/app/outputs/flutter-apk/app-release.apk /output/myhome.apk
+echo "=== Output After ==="
+ls -al /output
