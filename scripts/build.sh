740b71411b421249dc7483dc5919691bc6070896
천성원 <chun_sungwon@naver.com>
2026-07-19 21:45:30 +0900

    빌드 오류 수정#3

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 set -e
 cd /workspace
 flutter pub get
-flutter build apk --release --dart-define=COUCHDB_USER=${secrets.COUCHDB_USER} --dart-define=COUCHDB_PASSWORD=${secrets.COUCHDB_PASSWORD}
+flutter build apk --release --dart-define=COUCHDB_USER=${COUCHDB_USER} --dart-define=COUCHDB_PASSWORD=${COUCHDB_PASSWORD}
 cp build/app/outputs/flutter-apk/app-release.apk /output/myhome.apk
