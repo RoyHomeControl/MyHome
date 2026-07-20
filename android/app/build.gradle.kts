d7127cb6041a1af5cf1551da8883f7d13d37b43d
천성원 <chun_sungwon@naver.com>
2026-07-21 01:01:48 +0900

    빌드 에러 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -50,7 +51,7 @@ android {
             keyAlias = keystoreProperties.getProperty("keyAlias")
             keyPassword = keystoreProperties.getProperty("keyPassword")
             storePassword = keystoreProperties.getProperty("storePassword")
-            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storeFile = file(rootProject.file("android/${keystoreProperties.getProperty("storeFile")}"))
         }
     }
     
@@ -66,6 +67,10 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
