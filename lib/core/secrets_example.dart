eeb2b8ac8c305800725b8e2bf6c4838e8e569db4
roy752 <51858193+roy752@users.noreply.github.com>
2026-07-22 01:23:48 +0900

    Change isDev to boolean from string comparison

--- lib/core/secrets_example.dart
+++ lib/core/secrets_example.dart
@@ -2,5 +2,5 @@ const String couchdbUser =
     String.fromEnvironment('COUCHDB_USER', defaultValue: '');
 const String couchdbPassword =
     String.fromEnvironment('COUCHDB_PASSWORD', defaultValue: '');
-const String isDev =
-    String.fromEnvironment('IS_DEV', defaultValue: 'true');
+const bool isDev =
+    String.fromEnvironment('IS_DEV', defaultValue: 'true') == 'true';
